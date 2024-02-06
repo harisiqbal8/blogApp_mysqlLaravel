6408e7d955832ae1ce08d03907b8a1fdd9363096
harisiqbal8 <harisiqbaldec@gmail.com>
2024-02-06 18:01:19 +0500

    PR Review

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,12 +9,12 @@ class Comment extends Model
 {
     use HasFactory;
 
-    public function post() // FK:post_id
+    public function post()
     {
         return $this->belongsTo(Post::class);
     }
 
-    public function author() // FK:author_id
+    public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
