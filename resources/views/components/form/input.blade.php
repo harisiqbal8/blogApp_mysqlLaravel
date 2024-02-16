8ef46b65975a37658ce157f953b10edd1352cac3
harisiqbal8 <harisiqbaldec@gmail.com>
2024-02-16 19:29:50 +0500

    Admin Panel Developed

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,11 +1,14 @@
-@props(['name', 'type' => 'text', 'onkeypress' => '', 'onfocusout' => ''])
+@props(['name', 'onkeypress' => '', 'onfocusout' => ''])
 <div class="mb-6">
-    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="{{ $name }}">
-        {{ ucwords($name) }}
-    </label>
-    <input class="border border-gray-700 p-2 w-full" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}"
-        value="{{ old('$name') }}" onkeypress="{{ $onkeypress }}" onfocusout="{{ $onfocusout }}" required>
-    @error($name)
-        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-    @enderror
+    <x-form.label name="{{ $name }}" />
+    <input class="border border-gray-200 p-2 w-full rounded" 
+        name="{{ $name }}"
+        id="{{ $name }}" 
+        value="{{ old('$name') }}" 
+        onkeypress="{{ $onkeypress }}"
+        onfocusout="{{ $onfocusout }}" 
+        required
+        {{ $attributes }}
+    >
+    <x-form.error name="{{ $name }}" />
 </div>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,13 +1,14 @@
 @props(['name'])
 <div class="mb-6">
     <div class="flex mb-2 justify-between">
-        <label class="block uppercase font-bold text-xs text-gray-700" for="{{ $name }}">
-            {{ ucwords($name) }}
-        </label>
+        <x-form.label name="{{ $name }}" />
         {{ $slot }}
     </div>
-    <textarea class="border border-gray-700 p-2 w-full" type="textarea" name="{{ $name }}" id="{{ $name }}" required>{{ old($name) }}</textarea>
-    @error($name)
-        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-    @enderror
+    <textarea 
+        class="border border-gray-200 p-2 w-full rounded" 
+        type="textarea" 
+        name="{{ $name }}" 
+        id="{{ $name }}" 
+        required>{{ old($name) }}</textarea>
+    <x-form.error name="{{ $name }}" />
 </div>
